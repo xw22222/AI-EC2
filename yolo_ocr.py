40a05034d623baeeb662c188815d63a83a169a17
xw22222 <askoiu323@naver.com>
2023-01-29 19:28:30 +0900

    test4

--- yolo_ocr.py
+++ yolo_ocr.py
@@ -11,7 +11,7 @@ import time
 - 특정 folder 내에 있는 "가장 최근에 생성된" 파일을 리턴하는 방법 
 """
 folder_path = './input_img/'
-
+path = './crops'
 # each_file_path_and_gen_time: 각 file의 경로와, 생성 시간을 저장함
 each_file_path_and_gen_time = []
 for each_file_name in os.listdir(folder_path):
